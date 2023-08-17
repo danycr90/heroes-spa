7eed0107fe158bd8b3f4d99d58a5bcb88d79c97b
DanyCR90 <Leo.Castillo-Ramos@HenryScheinone.com>
2023-08-17 00:27:57 -0300

    Class 197

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,3 +1,9 @@
+import { Link } from "react-router-dom";
+
+const CharacterByHero = ({ alter_ego, characters }) => {
+    if ( alter_ego === characters ) return (<></>);
+    return <p>{ characters }</p>;
+}
 
 export const HeroCard = ({
     id,
@@ -31,7 +37,15 @@ export const HeroCard = ({
                             <h5 className="card-title">{ superhero }</h5>
                             <p className="card-text">{ alter_ego }</p>
 
-                            <p>{ characters }</p>
+                            <CharacterByHero characters={ characters } alter_ego={ alter_ego } />
+
+                            <p className="card-text">
+                                <small className="text-muted">{ first_appearance }</small>
+                            </p>
+
+                            <Link to={`/hero/${ id }`}>
+                                More...
+                            </Link>
 
                         </div>
 
