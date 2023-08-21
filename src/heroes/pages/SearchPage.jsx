2d6b8ce8c78edcf3b0a36e837b170e4c5b44f47f
DanyCR90 <Leo.Castillo-Ramos@HenryScheinone.com>
2023-08-20 22:06:24 -0300

    Class 203

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,31 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import { useForm } from "../../hooks/useForm"
 import { HeroCard } from "../components"
+import queryString from 'query-string';
 
 export const SearchPage = () => {
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  console.log(location);
+
+  const { q = ''} = queryString.parse( location.search );
+
+  const { searchText, onInputChange, onResetForm } = useForm({searchText: ''});
+
+  const handleSearchSubmit = ( event ) => {
+    event.preventDefault();
+
+    if ( searchText.trim().length <= 1 ) return;
+
+    navigate(`?q=${ searchText }`);
+
+    console.log({ searchText });
+
+    onResetForm();
+}
+
   return (
     <>
     <h1>Search</h1>
@@ -11,13 +36,15 @@ export const SearchPage = () => {
         <h4>Searching</h4>
         <hr />
 
-        <form action="">
+        <form onSubmit={ handleSearchSubmit }>
           <input 
             type="text" 
             placeholder="Search a hero" 
             className="form-control" 
             name="searchText" 
             autoComplete="off" 
+            value={ searchText }
+            onChange={ onInputChange }
           />
 
           <button 
@@ -36,7 +63,7 @@ export const SearchPage = () => {
         </div>
 
         <div className="alert alert-danger">
-          There's no results
+          There's no results for <b>{ q }</b>
         </div>
 
         {/* <HeroCard /> */}
